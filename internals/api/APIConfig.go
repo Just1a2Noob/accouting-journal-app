4b7f02a579478eae84681e4255988ede54ae1092
Just1a2Noob <kamikira.hd@gmail.com>
2025-11-12 16:49:29 +0700

    Updated APIConfig: Now saves a loggedin user

--- internals/api/APIConfig.go
+++ internals/api/APIConfig.go
@@ -4,7 +4,13 @@ import (
 	"github.com/Just1a2Noob/accouting-journal-app/internal/database"
 )
 
+type LoggedUser struct {
+	name  string
+	token string
+}
+
 type ApiConfig struct {
 	Database database.Queries
 	Secret   string
+	User     LoggedUser
 }
