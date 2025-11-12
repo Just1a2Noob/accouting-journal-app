7b0e2752a342d18b5d64f07801a69fb72c835813
Just1a2Noob <kamikira.hd@gmail.com>
2025-11-12 13:32:59 +0700

    Added CreateUsers queries

---
+++ sql/queries/users.sql
@@ -0,0 +1,19 @@
+-- name: CreateUsers :one
+INSERT INTO users (
+  id,
+  username,
+  created_at,
+  updated_at,
+  email,
+  section,
+  role
+) VALUES (
+  $1,
+  $2,
+  $3,
+  $4,
+  $5,
+  $6,
+  $7
+)
+RETURNING *;
