9272a06e8abf3ad2c848f9784ebd25b6776c01b2
Just1a2Noob <kamikira.hd@gmail.com>
2025-11-12 15:09:43 +0700

    Created a function to handle ErrorResponses

---
+++ internals/api/responses.go
@@ -0,0 +1,25 @@
+package api
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func ErrorResponse(w http.ResponseWriter, msg string, code int) {
+	response := struct {
+		ErrorMSG string
+	}{
+		ErrorMSG: msg,
+	}
+
+	errResponse, err := json.Marshal(response)
+	if err != nil {
+		log.Fatalf("Error marshalling json: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(code)
+	w.Write(errResponse)
+}
